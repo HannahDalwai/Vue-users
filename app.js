f475607f65c9a3c05200e50c6669e33b2b5be3a6
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-01-18 13:09:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const app = Vue.createApp({
+    data(){
+        return{
+                
+                users:[
+                    { name: "Hannah",
+                      surname: "Dalwai",
+                      age: "19",
+                    },
+                    { name: "Tony",
+                      surname: "ngxola",
+                      age: "25",
+                    },
+                    { name: "Darren",
+                      surname: "Petersen",
+                      age: "25",
+                    },
+                    { name: "Nadine",
+                      surname: "Roypen",
+                      age: "18",
+                    },
+                    { name: "Chloe",
+                    surname: "Petesen",
+                    age: "16",
+                  },
+                  { name: "Ingrid",
+                      surname: "Petesen",
+                      age: "74",
+                    },
+                    { name: "Liam",
+                      surname: "Dalwai",
+                      age: "22",
+                    },
+                    { name: "Hannah",
+                      surname: "Dalwai",
+                      age: "22",
+                    },
+                  
+                    
+                  
+                ],
+           
+        }
+    },
+    methods: {
+           
+                       
+    },
+})
+app.mount("#app");
